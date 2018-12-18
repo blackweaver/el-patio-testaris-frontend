7aed8232dd973afb83af2fc8940d0bb225841725
Marcos Irureta Goyena <marcos.irureta.goyena@gmail.com>
2018-12-18 18:44:19 -0300

    feat: reduce tasks to minimun

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,3 +1,4 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['sass', 'fonts', 'images', 'watch']);
+// gulp.task('default', ['sass', 'fonts', 'images', 'watch']);
+gulp.task('default', ['watch']);
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,12 +7,11 @@ var gulp        = require('gulp');
 var config      = require('../config');
 var browserSync = require('browser-sync');
 
-// gulp.task('watch', ['watchify', 'browserSync', 'scripts'], function() {
-gulp.task('watch', ['watchify', 'browserSync', 'scripts', 'concatJs'], function() {
-  gulp.watch(config.fonts.src, ['fonts']);
-  gulp.watch(config.images.src, ['images']);
-  gulp.watch(config.scripts.src + '/**', ['scripts']);
-  gulp.watch(config.sass.src, ['sass']);
+// gulp.task('watch', ['watchify', 'browserSync', 'scripts', 'concatJs'], function() {
+gulp.task('watch', ['watchify', 'browserSync'], function() {
+  // gulp.watch(config.fonts.src, ['fonts']);
+  // gulp.watch(config.images.src, ['images']);
+  // gulp.watch(config.scripts.src + '/**', ['scripts']);
+  // gulp.watch(config.sass.src, ['sass']);
   gulp.watch(config.php.src, ['php']);
-  // gulp.watch([config.production.jsDest  + '/**', '!' + config.production.jsDest  + '/scripts.js'], ['concatJs']);
 });
