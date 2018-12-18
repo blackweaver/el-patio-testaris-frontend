c8cde2e02c00ef29cf260eb2dc005421e015e66e
Marcos Irureta Goyena <marcos.irureta.goyena@gmail.com>
2018-12-18 18:05:26 -0300

    remove:util

--- gulp/tasks/uglifyJs.js
+++ gulp/tasks/uglifyJs.js
@@ -2,11 +2,9 @@ var gulp    = require('gulp');
 var config  = require('../config').production;
 var size    = require('gulp-filesize');
 var uglify = require('gulp-uglify');
-var gutil = require('gulp-util');
 
 gulp.task('uglifyJs', ['browserify'], function() {
   return gulp.src(config.jsSrc + 'scripts.js')
-    .pipe(uglify().on('error', gutil.log))
     .pipe(gulp.dest(config.jsDest))
     .pipe(size());
 });
