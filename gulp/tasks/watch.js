2b958b23bbbb67c47a226c20dc33ff7ee6673b54
Marcos Irureta Goyena <marcos.irureta.goyena@gmail.com>
2018-12-18 18:05:15 -0300

    rename

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,11 +7,12 @@ var gulp        = require('gulp');
 var config      = require('../config');
 var browserSync = require('browser-sync');
 
+// gulp.task('watch', ['watchify', 'browserSync', 'scripts'], function() {
 gulp.task('watch', ['watchify', 'browserSync', 'scripts', 'concatJs'], function() {
   gulp.watch(config.fonts.src, ['fonts']);
   gulp.watch(config.images.src, ['images']);
   gulp.watch(config.scripts.src + '/**', ['scripts']);
-  gulp.watch(config.sass.src,   ['scss']);
+  gulp.watch(config.sass.src, ['sass']);
   // gulp.watch(config.php.src, ['php']);
   // gulp.watch([config.production.jsDest  + '/**', '!' + config.production.jsDest  + '/scripts.js'], ['concatJs']);
 });
