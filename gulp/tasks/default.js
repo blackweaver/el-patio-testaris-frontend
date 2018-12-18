acc61a63229e886601dd8f31177eb33285f7f908
Marcos Irureta Goyena <marcos.irureta.goyena@gmail.com>
2018-12-18 18:47:00 -0300

    remove wp from static branch

--- gulp/tasks/default.js
+++
@@ -1,4 +0,0 @@
-var gulp = require('gulp');
-
-// gulp.task('default', ['sass', 'fonts', 'images', 'watch']);
-gulp.task('default', ['watch']);
---
+++ static_frontend/gulp/config.js
@@ -0,0 +1,60 @@
+var build = './dest';
+var dest = build;
+var src = './src';
+var fs = require('fs');
+var bstyles = require('bstyles');
+
+module.exports = {
+  build: build,
+  dest: dest,
+  src: src,
+  browserSync: {
+    host: 'localhost',
+    // proxy: 'http://local.testaris.ch',
+    open: 'external',
+    startPath: '/',
+    notify: true,
+    notify: {
+      styles: bstyles.BottomLeft
+    }
+  },
+  sass: {
+    src: src + '/sass/**/*.{sass,scss}',
+    dest: dest,
+    settings: {
+      indentedSyntax: true,
+      imagePath: 'images'
+    }
+  },
+  images: {
+    src: src + '/images/**',
+    dest: dest + '/images'
+  },
+  scripts: {
+    src: src + '/scripts',
+    dest: dest + '/js'
+  },
+  fonts: {
+    src: src + '/fonts/**',
+    dest: dest + '/fonts'
+  },
+  php: {
+    src: dest + '/**/*.php',
+    dest: dest
+  },
+  browserify: {
+    bundleConfigs: [
+      {
+        entries: src + '/scripts/scripts.js',
+        dest: dest + '/js',
+        outputName: 'scripts.js'
+      }
+    ]
+  },
+  production: {
+    cssSrc: dest + '/*.css',
+    jsSrc: dest + '/js/',
+    cssDest: dest,
+    jsDest: dest + '/js',
+  }
+};
