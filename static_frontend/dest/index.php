4459c993961dca7233d2fa33e0a384141ab206be
Marcos Irureta Goyena <marcos.irureta.goyena@gmail.com>
2018-12-29 14:31:05 -0300

    feat: remove duplicates and modularize

--- static_frontend/dest/index.php
+++ static_frontend/dest/index.php
@@ -9,14 +9,14 @@
                 <p>Tusdant vel ma quibusaped moluptas autatur autempora consendaera nam rerempos quam aut lit occumque estrumquam dolutem si am reheniet dolest que conseque pratur re plabore stiuscipis endignatur aut quia quatur,  que lame volorpori occulpa quiatumquam, simusam sedit voluptaera inctasi tatusam ani corum nullab iusaperum inus a volupta venet lique nectur aut dolor se susame occusanda. </p>
                 <p>Aquis magnihi litasinum quiatquis dolorio nsequam fuga.  Lore nis repudam, nem el et estinima sequodi tatque sequo volorehent voluptas inihillorum si que expla con quatuscium, nissin nimusandae intion nisciat accust vellum.</p>
             </div>
-            <div><a class="top" href="#"><img src="images/arrow-top.svg" alt=""></a></div>
+            <div class="back-to-top"><a class="top" href="#"><img src="images/arrow-top.svg" alt=""></a></div>
             <div class="fix-float"></div>
         </div>
     </section>
     <section class="text-box" id="dienstleistungen">
         <div>
             <div><h2>Dienstleistungen</h2></div>
-            <div>
+            <div class="grid-container">
                 <div class="box">
                     <img src="images/icon-magnifying.png" alt="">
                     <h4>Revision</h4>
@@ -36,14 +36,14 @@
                     <a href="javascript:;">> mehr erfahren</a> 
                 </div>
             </div>
-            <div><a class="top" href="#top"><img src="images/arrow-top.svg" alt=""></a></div>
+            <div class="back-to-top"><a class="top" href="#top"><img src="images/arrow-top.svg" alt=""></a></div>
             <div class="fix-float"></div>
         </div>
     </section>
     <section class="text-box" id="team">
         <div>
             <div><h2>Team</h2></div>
-            <div>
+            <div class="grid-container">
                 <div class="box-team">
                     <img src="images/team-picture.png" alt="">
                     <h4>Vorname Name</h4>
@@ -171,14 +171,14 @@
                     </p>
                 </div>
             </div>
-            <div><a class="top" href="#"><img src="images/arrow-top.svg" alt=""></a></div>
+            <div class="back-to-top"><a class="top" href="#"><img src="images/arrow-top.svg" alt=""></a></div>
             <div class="fix-float"></div>
         </div>
     </section>
     <section class="text-box" id="advisory-board">
         <div>
             <div><h2>Advisory Board</h2></div>
-            <div>
+            <div class="grid-container">
                 <div class="box-team">
                     <img src="images/team-picture.png" alt="">
                     <h4>Vorname Name</h4>
@@ -243,11 +243,11 @@
                     </p>
                 </div>
             </div>
-            <div><a class="top" href="#"><img src="images/arrow-top.svg" alt=""></a></div>
+            <div class="back-to-top"><a class="top" href="#"><img src="images/arrow-top.svg" alt=""></a></div>
             <div class="fix-float"></div>
         </div>
     </section>
-    <section class="text-box" id="kontakt">
+    <section class="text-box contact" id="kontakt">
         <div>
             <div><h2>Kontakt</h2></div>
             <div class="map">
@@ -263,7 +263,7 @@
                 <p>Lageplan:</p>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2692.749831684852!2d7.595061551328152!3d47.55319749955631!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4791b9b4976bfc35%3A0xf8a9df41e96d5f!2sMalzgasse+18%2C+4052+Basel%2C+Suiza!5e0!3m2!1ses!2suy!4v1545658514452" width="300" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
-            <div class="map"><a class="top" href="#"><img src="images/arrow-top.svg" alt=""></a></div>
+            <div class="map back-to-top"><a class="top" href="#"><img src="images/arrow-top.svg" alt=""></a></div>
             <div class="fix-float"></div>
         </div>
     </section>
